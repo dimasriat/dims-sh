304a692b623e3cfe2e88779b59b84f98e3ec12fc
Dimas <dimasriat@gmail.com>
2024-10-05 00:51:12 +0700

    feat: webpack

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'development', // or 'production'
+  entry: './src/index.ts',
+  output: {
+    path: path.resolve(__dirname, './web/js'),
+    filename: "dims-webpack.js" // <--- Will be compiled to this single file
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: [
+          {
+            loader: 'ts-loader',
+          },
+          {
+            loader: 'babel-loader',
+          },
+        ],
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.ts', '.js'],
+  },
+};
+
